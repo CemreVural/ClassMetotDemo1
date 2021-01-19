1f0349055ef3e3d15caa1b2425d27673a052cff9
CEMRE VURAL <vuralgsli@gmail.com>
2021-01-19 23:49:25 +0200

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+		public void Add(Customer customer)
+		{
+			Console.WriteLine(customer.Name+ " " + customer.LastName + " " + "Adlı Müşteri Eklendi");
+		}
+
+		public void Guncelle(string MusteriAdi, string MusteriSoyadi)
+		{
+			Console.WriteLine(MusteriAdi + " " + MusteriSoyadi + " " + "Adlı Müşteri güncellendi");
+		}
+
+		public void Delete(Customer customer)
+		{
+			Console.WriteLine(customer.Name + " " + customer.LastName + " " + "Adlı Müşteri Silindi");
+		}
+	}
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 357437130;
+            customer1.Name = "Cemre";
+            customer1.LastName = "Vural";
+            customer1.MailAdress = "vuralgsli@gmail.com";
+            customer1.Age = 23;
+            
+
+            Customer customer2 = new Customer();
+            customer2.Id = 367419870;
+            customer2.Name = "Mehmet";
+            customer2.LastName = "Alihan";
+            customer2.MailAdress = "alihan.mehmet@hotmail.com";
+            customer2.Age = 30;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 187766223;
+            customer3.Name = "Engin";
+            customer3.LastName = "Demirog";
+            customer3.MailAdress = "engin.demirog@hotmail.com";
+            customer3.Age = 29;
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+            customerManager.Add(customer3);
+        }
+    }
+}
